724f258e825720520717d9d91c002722d10b9d33
Marcus <mlim0032@student.monash.edu>
2021-09-14 12:33:42 +0800

    Initialise calculator instance in setUp method

--- test/pyunittest.py
+++ test/pyunittest.py
@@ -4,20 +4,20 @@ import unittest
 
 class TestCalculator(unittest.TestCase):
 
+    def setUp(self):
+        self.calculator = Calculator()
+
     # you may create more test methods
     # you may add parameters to test methods
     # this is an example
     def test_cost(self):
-        self.calculator = Calculator()
         # self.assertEqual(self.calculator.cost_calculation("", "", "", "", ""), "")
         self.assertEqual(round(self.calculator.cost_calculation(29, 37, 42, True, True),2), 0.28)
 
     def test_time(self):
-        self.calculator = Calculator()
         self.assertEqual(round(self.calculator.time_calculation(29,37,42,3.6),2), round(56/60,2))
 
     def test_holiday(self):
-        self.calculator = Calculator()
         # test public holiday
         self.assertEqual(self.calculator.is_holiday("25/12/2021"),True)
         # no holiday / weekdays
@@ -27,7 +27,7 @@ class TestCalculator(unittest.TestCase):
         # test school holidays ?
 
     def test_peak(self):
-        self.calculator = Calculator()
+        pass
 
     # you may create test suite if needed
     if __name__ == "__main__":
